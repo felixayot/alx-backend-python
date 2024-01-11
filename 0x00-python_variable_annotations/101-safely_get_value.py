9489090c8604dba0d01e8307ddc83b5083f96ae3
thinktank01 <felixayot@gmail.com>
2024-01-11 17:13:45 +0300

    Fix grammatical error in function docstring

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,7 @@ T = TypeVar('T')
 def safely_get_value(dct: Mapping, key: Any, default:
                      Union[T, None] = None) -> Union[Any, T]:
     """
-    Returns for a key in a given dictionary, dct,
+    Returns a queried key in a given dictionary, dct,
     otherwise, returns provided default.
     """
     if key in dct:
